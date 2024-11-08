421864859adf70fe734d25f28a835abc52f9b2fc
eshat69 <144534158+eshat69@users.noreply.github.com>
2024-11-09 01:37:13 +0600

    Add files via upload

---
+++ day9/arrRANDOM.PY.py
@@ -0,0 +1,10 @@
+import numpy as np
+a = np.random.rand(4) # generate el between 0 to 1
+print(a)
+b = np.random.rand(5)  # pos or neg close to 1
+print(b)
+c = np.random.ranf(4)   #
+print(c)
+d = np.random.randint(5,20,5)  #5 el range 5 to 20
+print(d)
+
