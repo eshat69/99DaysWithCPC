233d82be3876923f8801af45fbe20dc6355a4bc3
eshat69 <144534158+eshat69@users.noreply.github.com>
2024-11-23 00:10:49 +0600

    Add files via upload

---
+++ day23/hist_plot.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+x=[2,3,4,5,6,5,4,3,7,8,9,7,8,6]
+plt.hist(x,bins=20, edgecolor="black")
+plt.show()
---
+++ day23/violin_plot.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+a=[56,45,35.78,56,43,94,87,46,34,78,90]
+plt.violinplot(a,showmedians=True)
+plt.show()
